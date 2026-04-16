14899bc08a8172406dac498e7cf152af1a0c5941
Rakib5032 <rabbi15-5032@diu.edu.bd>
2026-04-16 20:38:24 +0600

    Use String output Parser

--- 7_chains/simple_chain.py
+++ 7_chains/simple_chain.py
@@ -1,28 +1,27 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import PydanticOutputParser
-from pydantic import BaseModel, Field
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
+
+template = PromptTemplate(
+    template='Give 5 interesting fun fact about {topic} ',
+    input_variables=['topic'],
+)
 
 model = ChatOpenAI(
     model= 'gpt-4o-mini',
     base_url="https://openrouter.ai/api/v1"
 )
 
-class Fact(BaseModel):
-    facts: list[str] = Field(description='5 Facts about the topic')
-    
-parser = PydanticOutputParser(pydantic_object=Fact)
+parser = StrOutputParser()
 
-template = PromptTemplate(
-    template='Give 5 interesting fun fact about {topic} \n{format_instruction}',
-    input_variables=['topic'],
-    partial_variables={'format_instruction': parser.get_format_instructions()}
-)
-
+# chain
 chain = template | model | parser
+
 prompt = {'topic': 'Football'}
 result = chain.invoke(prompt)
 print(result)
+
+chain.get_graph().print_ascii()
