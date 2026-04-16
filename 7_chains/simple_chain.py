49ca2deb0d42a3ff036c803d4155de93b713eb7e
Rakib5032 <rabbi15-5032@diu.edu.bd>
2026-04-16 20:15:52 +0600

    Simple Chain

--- 7_chains/simple_chain.py
+++ 7_chains/simple_chain.py
@@ -1,6 +1,8 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
@@ -9,5 +11,18 @@ model = ChatOpenAI(
     base_url="https://openrouter.ai/api/v1"
 )
 
-response = model.invoke('tell about tcl global')
-print(response.content)
+class Fact(BaseModel):
+    facts: list[str] = Field(description='5 Facts about the topic')
+    
+parser = PydanticOutputParser(pydantic_object=Fact)
+
+template = PromptTemplate(
+    template='Give 5 interesting fun fact about {topic} \n{format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+prompt = {'topic': 'Football'}
+result = chain.invoke(prompt)
+print(result)
